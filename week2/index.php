2ab9086dfbd9da7eb565776b74823895dbcd0077
Tarun Shokeen <shokeentarun20@gmail.com>
2025-05-14 16:34:03 -0400

    added week2 file

---
+++ week2/index.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+
+    <?php
+
+    echo "<h1>Hello World!</h1>";
+    echo "<p1>My Name is Tarun</p>";
+
+    ?>
+    <hr>
+
+    <?php
+
+        $fname = "Tarun";
+        $lname = "Shokeen";
+
+        echo "<h1>Hello $fname $lname</h1>";
+
+    ?>
+
+</body>
+</html>
